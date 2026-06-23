44bafe5e5983346183c5e27ddefeba4109440480
Abdulla-Aldosari <alwafi.go@gmail.com>
2026-06-23 22:02:45 +0300

    chore(commitlint): enforce non-empty scope in commit messages
    Add a `scope-empty` rule set to error level to require a scope in all
    commit messages, ensuring consistent and descriptive commit formatting.

--- commitlint.config.js
+++ commitlint.config.js
@@ -4,4 +4,9 @@
 /** @type {import('@commitlint/types').UserConfig} */
 module.exports = {
   extends: ["@commitlint/config-conventional"],
+  rules: {
+    // `2` = error (refuses to commit)
+    // "never" = The scope is never allowed to be empty
+    "scope-empty": [2, "never"],
+  },
 };
